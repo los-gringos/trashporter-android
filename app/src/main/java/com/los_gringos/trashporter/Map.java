46002e6fb297a70fe3f626aa0c64bdce80bda094
kostasmantz <kostiksm@gmail.com>
2018-05-05 15:57:21 +0300

    Added the markers in the Map

--- app/src/main/java/com/los_gringos/trashporter/Map.java
+++ app/src/main/java/com/los_gringos/trashporter/Map.java
@@ -25,10 +25,37 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng serres = new LatLng(41.092083, 23.541016);
+        LatLng provatas = new LatLng(41.068238, 23.390686);
+        LatLng anwKamila = new LatLng(41.058320, 23.424134);
+        LatLng katwKamila = new LatLng(41.020431, 23.483293);
+        LatLng mitrousi = new LatLng(41.058680, 23.457547);
+        LatLng koumaria = new LatLng(41.016434, 23.434656);
+        LatLng skoutari = new LatLng(41.020032, 23.520701);
+        LatLng adelfiko = new LatLng(41.014645, 23.457354);
+        LatLng agiaEleni = new LatLng(41.003545, 23.559196);
+        LatLng peponia = new LatLng(40.988154, 23.516756);
+        googleMap.addMarker(new MarkerOptions().position(serres)
+                .title("Marker in Serres"));
+        googleMap.addMarker(new MarkerOptions().position(provatas)
+                .title("Marker in provatas"));
+        googleMap.addMarker(new MarkerOptions().position(anwKamila)
+                .title("Marker in Anw Kamila"));
+        googleMap.addMarker(new MarkerOptions().position(katwKamila)
+                .title("Marker in Katw Kamila"));
+        googleMap.addMarker(new MarkerOptions().position(mitrousi)
+                .title("Marker in Mitrousi"));
+        googleMap.addMarker(new MarkerOptions().position(koumaria)
+                .title("Marker in Koumaria"));
+        googleMap.addMarker(new MarkerOptions().position(skoutari)
+                .title("Marker in Skoutari"));
+        googleMap.addMarker(new MarkerOptions().position(adelfiko)
+                .title("Marker in Adelfiko"));
+        googleMap.addMarker(new MarkerOptions().position(agiaEleni)
+                .title("Marker in Agia Eleni"));
+        googleMap.addMarker(new MarkerOptions().position(peponia)
+                .title("Marker in Peponia"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(serres));
 
     }
 }
