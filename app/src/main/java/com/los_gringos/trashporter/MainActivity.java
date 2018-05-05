268e23e863b1f28edd177309173487a9c4748d89
kostasmantz <kostiksm@gmail.com>
2018-05-05 15:05:18 +0300

    Implemented a basic Map Activity

--- app/src/main/java/com/los_gringos/trashporter/MainActivity.java
+++ app/src/main/java/com/los_gringos/trashporter/MainActivity.java
@@ -1,13 +1,29 @@
 package com.los_gringos.trashporter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button openMapsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openMapsButton = findViewById(R.id.mainOpenMapsButton);
+        openMapsButton.setOnClickListener(openMapsButtonClicked);
     }
+
+    View.OnClickListener openMapsButtonClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(getBaseContext(),Map.class));
+        }
+    };
 }
---
+++ app/src/main/java/com/los_gringos/trashporter/Map.java
@@ -0,0 +1,34 @@
+package com.los_gringos.trashporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+}
