5e23b5012a5cdc48de0014e673750cdffd5b68a5
eLGreko21 <miguel.daios@hotmail.com>
2018-05-06 03:22:30 +0300

    Implemented file import layout.

--- app/src/main/java/io/los/gringos/trashporter/android/activity/Map.java
+++ app/src/main/java/io/los/gringos/trashporter/android/activity/Map.java
@@ -3,19 +3,26 @@ package io.los.gringos.trashporter.android.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.los.gringos.trashporter.R;
-import io.los.gringos.trashporter.android.utils.LocationUtil;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
-
+    public List<Marker> markers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,24 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapFragment);
-        mapFragment.getMapAsync(this);
 
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //Creates Markers on Map and inserts them to List markers.
+        this.addMarkers(googleMap);
+
+        //zooms around the markers field when map opens
+        this.zoomOnMarkers(googleMap);
+
+        this.setPolyliners(googleMap);
+
+        // googleMap.moveCamera(CameraUpdateFactory.newLatLng(serres));
+    }
+
+    public void addMarkers(GoogleMap googleMap) {
         LatLng serres = new LatLng(41.092083, 23.541016);
         LatLng provatas = new LatLng(41.068238, 23.390686);
         LatLng anwKamila = new LatLng(41.058320, 23.424134);
@@ -41,31 +60,66 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
         LatLng agiaEleni = new LatLng(41.003545, 23.559196);
         LatLng peponia = new LatLng(40.988154, 23.516756);
 
-
-        
-
-        int i = 5;
-
-        googleMap.addMarker(new MarkerOptions().position(serres)
+        Marker serresMarker = googleMap.addMarker(new MarkerOptions().position(serres)
                 .title("Marker in Serres"));
-        googleMap.addMarker(new MarkerOptions().position(provatas)
+        Marker provatasMarker = googleMap.addMarker(new MarkerOptions().position(provatas)
                 .title("Marker in provatas"));
-        googleMap.addMarker(new MarkerOptions().position(anwKamila)
+        Marker anwKamilaMarker = googleMap.addMarker(new MarkerOptions().position(anwKamila)
                 .title("Marker in Anw Kamila"));
-        googleMap.addMarker(new MarkerOptions().position(katwKamila)
+        Marker katwKamilaMarker = googleMap.addMarker(new MarkerOptions().position(katwKamila)
                 .title("Marker in Katw Kamila"));
-        googleMap.addMarker(new MarkerOptions().position(mitrousi)
+        Marker mitrousiMarker = googleMap.addMarker(new MarkerOptions().position(mitrousi)
                 .title("Marker in Mitrousi"));
-        googleMap.addMarker(new MarkerOptions().position(koumaria)
+        Marker koumariaMarker = googleMap.addMarker(new MarkerOptions().position(koumaria)
                 .title("Marker in Koumaria"));
-        googleMap.addMarker(new MarkerOptions().position(skoutari)
+        Marker skoutariMarker = googleMap.addMarker(new MarkerOptions().position(skoutari)
                 .title("Marker in Skoutari"));
-        googleMap.addMarker(new MarkerOptions().position(adelfiko)
+        Marker adelfikoMarker = googleMap.addMarker(new MarkerOptions().position(adelfiko)
                 .title("Marker in Adelfiko"));
-        googleMap.addMarker(new MarkerOptions().position(agiaEleni)
+        Marker agiaEleniMarker = googleMap.addMarker(new MarkerOptions().position(agiaEleni)
                 .title("Marker in Agia Eleni"));
-        googleMap.addMarker(new MarkerOptions().position(peponia)
+        Marker peponiaMarker = googleMap.addMarker(new MarkerOptions().position(peponia)
                 .title("Marker in Peponia"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(serres));
+
+        markers.add(serresMarker);
+        markers.add(provatasMarker);
+        markers.add(anwKamilaMarker);
+        markers.add(katwKamilaMarker);
+        markers.add(mitrousiMarker);
+        markers.add(koumariaMarker);
+        markers.add(skoutariMarker);
+        markers.add(adelfikoMarker);
+        markers.add(agiaEleniMarker);
+        markers.add(peponiaMarker);
+    }
+
+    public void zoomOnMarkers(GoogleMap googleMap) {
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        for (Marker marker : markers) {
+            builder.include(marker.getPosition());
+        }
+
+        LatLngBounds bounds = builder.build();
+
+        int padding = 150; // offset from edges of the map in pixels
+
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+
+        googleMap.moveCamera(cu);
+
+        googleMap.animateCamera(cu);
+    }
+
+    public void setPolyliners(GoogleMap googleMap) {
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(41.092083, 23.541016),
+                        new LatLng(41.068238, 23.390686),
+                        new LatLng(41.058320, 23.424134),
+                        new LatLng(41.020431, 23.483293),
+                        new LatLng(41.058680, 23.457547),
+                        new LatLng(41.016434, 23.434656)));
     }
 }
